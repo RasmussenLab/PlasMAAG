daf9939447ef2bbb40a97843880a142ed558f1ad
Paupiera <pau.piera.linde@gmail.com>
2025-12-03 14:46:38 +0100

    split clusters per sample when writing bins

--- files_used_in_snakemake_workflow/write_candidate_bins_virus.py
+++ files_used_in_snakemake_workflow/write_candidate_bins_virus.py
@@ -6,6 +6,22 @@ from git_commit import get_git_commit
 import gzip
 import numpy as np 
 
+
+def split_clusters_by_sample(clusters):
+    cls_split = dict()
+    for i, (cl_id, cl_cs) in enumerate(clusters.items()):
+        samples_in_cl = set([c.split("C")[0] for c in cl_cs])
+
+        cl_S_d = {S: set() for S in samples_in_cl}
+
+        for c in cl_cs:
+            cl_S_d[c.split("C")[0]].add(c)
+
+        for S, cs in cl_S_d.items():
+            cls_split["%s_%s" % (str(cl_id), S)] = cs
+
+    return cls_split
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Write plasmid and non plasmid bins")
 
@@ -79,7 +95,7 @@ if __name__ == "__main__":
     print(args)
 
     # 
-    plcl_cs_d = { cl:set() for cl,c in np.loadtxt(args.cls_pl,dtype=object,skiprows=1)}
+    plcl_cs_d = split_clusters_by_sample({ cl:set() for cl,c in np.loadtxt(args.cls_pl,dtype=object,skiprows=1)})
     for cl,c in np.loadtxt(args.cls_pl,dtype=object,skiprows=1):
         plcl_cs_d[cl].add(c)
     plcl_len_d = { cl:np.sum([ int(c.split("length_")[1].split("_")[0]) for c in cs]) for cl,cs in plcl_cs_d.items() }
@@ -87,7 +103,7 @@ if __name__ == "__main__":
     pl_cs = set(c2plcl.keys())
     
     
-    orgcl_cs_d = { cl:set() for cl,c in np.loadtxt(args.cls_org,dtype=object,skiprows=1)}
+    orgcl_cs_d = split_clusters_by_sample({ cl:set() for cl,c in np.loadtxt(args.cls_org,dtype=object,skiprows=1)})
     for cl,c in np.loadtxt(
         args.cls_org,dtype=object,skiprows=1):
         orgcl_cs_d[cl].add(c)
@@ -96,7 +112,7 @@ if __name__ == "__main__":
     c2orgcl={ c:cl for cl,cs in orgcl_cs_d.items() if orgcl_len_d[cl] >= args.min_org_len for c in cs}
     org_cs = set(c2orgcl.keys())
 
-    vircl_cs_d = { cl:set() for cl,c in np.loadtxt(args.cls_vir,dtype=object,skiprows=1)}
+    vircl_cs_d = split_clusters_by_sample({ cl:set() for cl,c in np.loadtxt(args.cls_vir,dtype=object,skiprows=1)})
     for cl,c in np.loadtxt(
         args.cls_vir,dtype=object,skiprows=1):
         vircl_cs_d[cl].add(c)
