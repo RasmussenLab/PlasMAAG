8fdcb5a4bc3335bb10355a1726055ae3d5dd3749
Paupiera <pau.piera.linde@gmail.com>
2026-04-15 12:24:06 +0200

    enable to merge even when no circular contigs present

--- files_used_in_snakemake_workflow/merge_circular_plamb_clusters.py
+++ files_used_in_snakemake_workflow/merge_circular_plamb_clusters.py
@@ -38,12 +38,14 @@ if __name__ == "__main__":
     print(args)
 
     ## merge clusters
-    circular_contigs_arr = np.loadtxt(args.cls_circular, dtype=object)
-    if circular_contigs_arr.shape[0] == 1:
+    circular_contigs_arr = np.loadtxt(args.cls_circular, dtype=object,skiprows=1)
+    
+    if circular_contigs_arr.shape[0] == 0:
         circular_contigs = set()
     else:
-        cls_plamb_arr = np.loadtxt(args.cls_plamb, skiprows=1, dtype=object)
-        circular_contigs = set(circular_contigs_arr[1:, 1])
+        circular_contigs = set(circular_contigs_arr[:, 1])
+    
+    cls_plamb_arr = np.loadtxt(args.cls_plamb, skiprows=1, dtype=object)
 
     with open(args.outcls, "w") as f:
         f.write("%s\t%s\n" % ("clustername", "contigname"))
